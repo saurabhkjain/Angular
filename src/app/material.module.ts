e050f40bcd3f9cbb6d16f327c3d4291d92525d35
Saurabh Jain <JainS@upco.co.uk>
2018-02-23 16:56:20 +0000

    Added elipsis

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatButtonModule, MatToolbarModule, MatInputModule, MatProgressSpinnerModule, MatCardModule, MatSnackBarModule } from '@angular/material';
+import { MatButtonModule, MatToolbarModule, MatInputModule, MatProgressSpinnerModule, MatCardModule, MatSnackBarModule, MatMenuModule, MatIconModule } from '@angular/material';
 
 @NgModule({
-    imports: [MatButtonModule, MatToolbarModule, MatInputModule, MatProgressSpinnerModule, MatCardModule, MatSnackBarModule],
-    exports: [MatButtonModule, MatToolbarModule, MatInputModule, MatProgressSpinnerModule, MatCardModule, MatSnackBarModule]
+    imports: [MatButtonModule, MatToolbarModule, MatInputModule, MatProgressSpinnerModule, MatCardModule, MatSnackBarModule, MatMenuModule, MatIconModule],
+    exports: [MatButtonModule, MatToolbarModule, MatInputModule, MatProgressSpinnerModule, MatCardModule, MatSnackBarModule, MatMenuModule, MatIconModule]
 })
 
 export class MaterialModule { }
